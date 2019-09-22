7c8eea3a0c31a133e7884dfad2adba1ec7b8a608
lianghongyu <2388638093@qq.com>
2019-09-22 20:29:25 +0800

    20190922提交，没有实现命令按钮控制功能

---
+++ jidianqi/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace jidianqi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comb_baud = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comb_port = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.button11 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.comb_baud);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comb_port);
+            this.groupBox1.Location = new System.Drawing.Point(61, 29);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(436, 572);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "控制状态";
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(44, 292);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 57);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "扫描串口";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(44, 486);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 57);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "断开所有";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(44, 357);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开串口";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 421);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 57);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "吸合所有";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comb_baud
+            // 
+            this.comb_baud.FormattingEnabled = true;
+            this.comb_baud.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "43000",
+            "56000",
+            "115200"});
+            this.comb_baud.Location = new System.Drawing.Point(130, 159);
+            this.comb_baud.Margin = new System.Windows.Forms.Padding(4);
+            this.comb_baud.Name = "comb_baud";
+            this.comb_baud.Size = new System.Drawing.Size(182, 32);
+            this.comb_baud.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 167);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "波特率：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "串口号：";
+            // 
+            // comb_port
+            // 
+            this.comb_port.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comb_port.FormattingEnabled = true;
+            this.comb_port.Location = new System.Drawing.Point(130, 66);
+            this.comb_port.Margin = new System.Windows.Forms.Padding(4);
+            this.comb_port.Name = "comb_port";
+            this.comb_port.Size = new System.Drawing.Size(182, 32);
+            this.comb_port.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(545, 29);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(436, 572);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "输出状态";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(309, 298);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(102, 45);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "关";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(171, 298);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(102, 45);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "开";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 308);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "滚刷堵转";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(309, 181);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(102, 45);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "关";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(171, 181);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(102, 45);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "开";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(309, 64);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(102, 45);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "关";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(171, 64);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(102, 45);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "开";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 191);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "边刷堵转";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 74);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "边轮堵转";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(232, 357);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(186, 57);
+            this.button11.TabIndex = 3;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 635);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comb_baud;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comb_port;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ jidianqi/Form1.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace jidianqi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //我们在form载入时，扫描识别存在的串口。
+            Scan_serialport(serialPort, comb_port);  //调用扫描串口函数
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);//必须手动添加事件处理程序
+            comb_baud.Text = "115200";
+            button11.BackColor = Color.Red;
+            button11.Text = "串口已关闭";
+        }
+
+        //串口接收函数，需要定义数据接收的格式
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(serialPort.IsOpen)
+            {
+                try
+                {
+                    serialPort.Close();
+                    button1.Text = "打开串口";
+                    button4.Enabled = true;
+                    button11.BackColor = Color.Red;
+                    button11.Text = "串口已关闭";
+                }
+                catch
+                {
+                    MessageBox.Show("关闭串口失败","错误");
+                }
+            }
+            else
+            {
+                try
+                {
+                    serialPort.PortName = comb_port.Text;
+                    serialPort.BaudRate = Convert.ToInt32(comb_baud.Text, 10);//设置波特率
+                    serialPort.Open();
+                    button1.Text = "关闭串口";
+                    button4.Enabled = false;
+                    button11.BackColor = Color.Green;
+                    button11.Text = "串口已打开";
+                }
+                catch
+                {
+                    MessageBox.Show("打开串口失败", "错误");
+                }
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Scan_serialport(serialPort, comb_port);  //调用扫描串口函数
+        }
+
+        //实现串口扫描函数
+        private void Scan_serialport(SerialPort myport,ComboBox myport_combobox)
+        {
+            string buffer;
+            string[] com_string = new string[15];
+            int count = 0;
+            myport_combobox.Items.Clear();
+            for (int i = 1; i < 15; i++)
+            {
+                try
+                {
+                    buffer = "COM" + i.ToString();
+                    myport.PortName = buffer;
+                    myport.Open();
+                    com_string[count] = buffer;
+                    myport_combobox.Items.Add(buffer);
+                    myport.Close();
+                }
+                catch{}
+            }
+            myport_combobox.Text = com_string[0];
+        }
+
+        //实现数据（控制指令）的发送函数
+        //参数列表：数据头，地址，功能码，四字节数据，校验和
+        private void write_command_to_serialport(SerialPort myport,byte data)
+        {
+            byte[] command = new byte[8];//需重新定义命令的格式
+            if(myport.IsOpen)
+            {
+                try
+                {
+                    myport.Write(command, 0, 8);
+                }
+                catch
+                {
+                    MessageBox.Show("命令发送失败，请检查", "错误");
+                }
+            }
+        }
+    }
+}
